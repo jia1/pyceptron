2bda667bac3886774a29b593251b7f0cd2cf5191
Jia Yee <jiayeerawr@gmail.com>
2017-11-06 11:39:40 +0800

    Tokenize by space and remove stop words
    Might need to strip punctuation from tokens
    
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- preproc.py
+++ preproc.py
@@ -3,6 +3,15 @@ import os
 src_dir = os.path.abspath('tc')
 dst_dir = os.path.abspath('tc_proc')
 
+with open('stopword-list', 'r') as s:
+    stop_list = list(map(lambda ln: ln.strip(), s.readlines()))
+
+def rm_stop(ln):
+    tokens = ln.split(' ')
+    return list(filter(lambda t: t not in stop_list, tokens))
+
 for curr_dir, sub_dir, files in os.walk(src_dir):
+    curr_class = curr_dir.split('/')[-1]
     for file in files:
-        pass
+        with open(os.path.join(curr_dir, file), 'r') as f:
+            filtered_lines = list(map(lambda ln: rm_stop(ln.strip()), f.readlines()))
