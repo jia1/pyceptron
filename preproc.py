801498fe4f846fa4bd5e576ee3648c5c98bc4988
Jia Yee <jiayeerawr@gmail.com>
2017-11-08 16:32:54 +0800

    Decide k to be 2
    1. If k = 2 then only 31 frequency dictionaries are empty
    2. If k = 3 then 400+ frequency dictionaries are empty
    
    Each class (c1, c2, ...) has 500 texts. There are 5 classes
    
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- preproc.py
+++ preproc.py
@@ -18,20 +18,30 @@ def strip_and_filter_line(ln):
     return list(filter(lambda t: t and len(t) > 2 and t.isalpha() and t not in stop_list, tokens))
 
 p = PorterStemmer()
+k = 2
+
+num_empty_dict = 0
 
 for curr_dir, sub_dir, files in os.walk(src_dir):
     curr_class = curr_dir.split('/')[-1]
     for file in files:
         flat_text = []
+        freq_dict = {}
         with open(os.path.join(curr_dir, file), 'r') as f:
             processed_lines = map(lambda ln: strip_and_filter_line(ln), f.readlines())
             for line in processed_lines:
               flat_text.extend(list(map(lambda word: p.stem(word, 0, len(word) - 1), line)))
-            print(flat_text)
-            print(file)
-            break
-    if files:
-      break
+            for word in flat_text:
+              if word not in freq_dict:
+                freq_dict[word] = 1
+              else:
+                freq_dict[word] += 1
+            fin_freq_dict = { word: freq for word, freq in freq_dict.items() if freq >= k }
+            if not fin_freq_dict:
+              num_empty_dict += 1
+              print(file, end = ' ')
+
+print(num_empty_dict)
 
 # translator = str.maketrans('', '', string.punctuation)
 # some_string.translate(translator)
