f90628576c7a8cd5667afad01c55a19a0d97151a
Jia Yee <jiayeerawr@gmail.com>
2017-11-10 03:17:16 +0800

    Fix indent and copy program over to ipynb
    ipynb has the completed feature selection algorithm
    
    But might still need some fixing
    
    Signed-off-by: Jia Yee <jiayeerawr@gmail.com>

--- preproc.py
+++ preproc.py
@@ -18,14 +18,14 @@ def strip_and_filter_line(ln):
     return list(filter(lambda t: t and len(t) > 2 and t.isalpha() and t not in stop_list, tokens))
 
 def chi_square(w, c):
-  n00 = 0
-  n01 = 0
-  n10 = 0
-  n11 = 0
-  return ((n11+n10+n01+n00)*(n11*n00-n10*n01)**2)/((n11+n01)*(n11+n10)*(n10+n00)*(n01+n00))
+    n00 = 0
+    n01 = 0
+    n10 = 0
+    n11 = 0
+    return ((n11+n10+n01+n00)*(n11*n00-n10*n01)**2)/((n11+n01)*(n11+n10)*(n10+n00)*(n01+n00))
 
 def feature_select(d):
-  pass
+    pass
 
 p = PorterStemmer()
 k = 2
@@ -38,15 +38,15 @@ for curr_dir, sub_dir, files in os.walk(src_dir):
         with open(os.path.join(curr_dir, file), 'r') as f:
             processed_lines = map(lambda ln: strip_and_filter_line(ln), f.readlines())
             for line in processed_lines:
-              flat_text.extend(list(map(lambda word: p.stem(word, 0, len(word) - 1), line)))
+                flat_text.extend(list(map(lambda word: p.stem(word, 0, len(word) - 1), line)))
             for word in flat_text:
-              if word not in freq_dict:
-                freq_dict[word] = 1
-              else:
-                freq_dict[word] += 1
+                if word not in freq_dict:
+                    freq_dict[word] = 1
+                else:
+                    freq_dict[word] += 1
             fin_freq_dict = { word: freq for word, freq in freq_dict.items() if freq >= k }
             if not fin_freq_dict:
-              fin_freq_dict = freq_dict
+                fin_freq_dict = freq_dict
             # Normalize the frequencies based on fin_freq_dict
             sum_freq = sum(fin_freq_dict.values())
             normalized_freq_dict = { word: freq / sum_freq for word, freq in fin_freq_dict.items() }
